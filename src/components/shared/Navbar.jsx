866e1f2291cb3c93e2b82bdd3b980ea2ceee0ea7
emerson <xinayespinoza@gmail.com>
2025-05-02 19:12:23 -0400

    fix: acordeon con css

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -39,28 +39,54 @@ const NavBar = () => {
       </header>
 
       <section className="platform-description">
-        <div className="platform-description-container">
-          <div className="card platform-card platform-card--intro">
-            <h2 className="card-title">¿Qué es CompiTV?</h2>
-            <p className="card-text">
-              CompiTV es una plataforma global que conecta a las audiencias con canales de señal abierta, así como con canales que buscan ampliar su visibilidad internacional. Accede fácilmente a contenido en vivo y diverso desde cualquier parte del mundo.
-            </p>
-            <img src={portada} alt="Portada CompiTV" className="img-portada" />
+        <div className="platform-description-container acordeon">
+
+          {/* Item 1 */}
+          <div className="acordeon-item">
+            <input type="checkbox" id="intro" />
+            <label htmlFor="intro" className='que_es_compi'>¿Qué es CompiTV?</label>
+            <div className="content">
+              <div className="card platform-card platform-card--intro">
+                <p className="card-text">
+                  CompiTV es una plataforma global que conecta a las audiencias con canales de señal abierta, así como con canales que buscan ampliar su visibilidad internacional. Accede fácilmente a contenido en vivo y diverso desde cualquier parte del mundo.
+                </p>
+                <img src={portada} alt="Portada CompiTV" className="img-portada" />
+              </div>
+            </div>
           </div>
 
-          <div className="card platform-card platform-card--steps">
-            <h2 className="card-title">¿Cómo funciona?</h2>
-            <ul className="platform-description-steps">
-              <li><strong>1. Selecciona tu país:</strong> Desde el mapa 3D interactivo o mediante el menú desplegable.</li>
-              <li><strong>2. Explora los canales:</strong> Se mostrarán todos los canales disponibles para ese país.</li>
-              <li><strong>3. Cambia de canal:</strong> Usa los botones "Siguiente" y "Atrás".</li>
-              <li><strong>4. Usa la búsqueda:</strong> Localiza un canal por nombre fácilmente.</li>
-              <li><strong>5. Guarda tus favoritos:</strong> Haz clic en el ícono para conservar tus canales preferidos y acceder a ellos más rápido la próxima vez.</li>
-            </ul>
+          {/* Item 2 */}
+          <div className="acordeon-item">
+            <input type="checkbox" id="funciona" />
+            <label htmlFor="funciona" className='como_funciona'>¿Cómo funciona?</label>
+            <div className="content">
+              <div className="card platform-card platform-card--steps">
+                <ul className="platform-description-steps">
+                  <li>
+                    <strong>1. Selecciona tu país:</strong> Elige tu ubicación desde el mapa 3D interactivo o utilizando el menú desplegable para explorar canales disponibles.
+                  </li>
+                  <li>
+                    <strong>2. Explora los canales:</strong> Visualiza todos los canales abiertos disponibles en el país seleccionado, organizados para facilitar tu navegación.
+                  </li>
+                  <li>
+                    <strong>3. Cambia de canal:</strong> Navega entre canales fácilmente usando los botones “Siguiente” y “Atrás”.
+                  </li>
+                  <li>
+                    <strong>4. Usa la búsqueda:</strong> Encuentra rápidamente cualquier canal escribiendo su nombre en el buscador.
+                  </li>
+                  <li>
+                    <strong>5. Guarda tus favoritos:</strong> Haz clic en el ícono de favorito para guardar los canales que más te gustan y acceder a ellos más rápido en futuras visitas.
+                  </li>
+                </ul>
+
+
+              </div>
+            </div>
           </div>
+
         </div>
-
       </section>
+
     </>
   );
 };
